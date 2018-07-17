7adb82c221ac4929a7f363d6d792465a6d1c19e8
Chase Maguire <itlm023@iet-ats.ucdavis.edu>
2018-07-17 16:35:43 -0700

    More logic changes.

--- protein2sat.py
+++ protein2sat.py
@@ -1,21 +1,20 @@
 
 """Written in python 3.6"""
-#Getting input
+# Getting input
 i = list(input("Enter input string \n"))
-#Slice the input into a list
+# Slice the input into a list
 # A count of how many variables we have
 varcount = 0
 """Placement rules"""
 
-'''Rule 1; Each acid must be placed'''
-#Let there be a variable, X(i,j), is either 1 or 0; true or false, if character I is placed in position J
-#i goes from 1 to n, j goes to  1 to n^2
+# Let there be a variable, X(i,j), is either 1 or 0; true or false, if character I is placed in position J
+# i goes from 1 to n, j goes to  1 to n^2
 j = []
 for x in range((len(i)*len(i))):
     j.append(x)
 
-print(i)
-print(j)
+'''Rule 1; Each acid must be placed'''
+
 # Each spot needs to be placed
 # Creation of X(i,j) represented as a 2d array
 # We just need to specify that each i will be placed eventualy
@@ -26,21 +25,31 @@ print(j)
 # Matrix = [[0 for x in range(w)] for y in range(h)] 
 # First array slice denotes the place in the grid, second denotes the place in the input string 
 Mat = [[0 for x in range(len(i))] for y in range(len(j))]
-print(Mat)
+#print(Mat)
 # for each place in the grid, for each position, create the placement rule
 # Creating a way to store each on of these placement rules
-uniquelist = []
+placementlist = []
 for x in range(len(i)):
+    varcount = 0
     currentlist = []
     for y in range(len(j)):
         # Need to generate a new variable here
         varcount = varcount + 1
         currentlist.append(varcount)
     currentlist.append(0)
-    uniquelist.append(currentlist)
-print(uniquelist)
+    placementlist.append(currentlist)
+print(placementlist)
 # Now, each slot j has i variables 'assigned' to it. 
-# Just needs to be written to the file, but this is the last step\
+# Just needs to be written to the file, but this is the last step
 ''' One acid per grid '''
-# 
+# Should be something such as 
+# For each j, for each i
+#  
+
 ''' Rule 3, each acid must be placed adjacent to the previously placed one. The first is the exception '''
+# For i, but not the first
+# i+1 must be in either
+#   the grid to the bottom, top , left, or right
+#   Exceptions are on the edges.
+# Corners are 0, n, n^2, n^2-n
+
